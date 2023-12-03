5779d8d9b13d73f3d27d9cbf0a4946bd7084d940
sagishiva <152669813+sagishiva@users.noreply.github.com>
2023-12-03 14:24:39 +0530

    Update settings.py

--- furnitureapp/settings.py
+++ furnitureapp/settings.py
@@ -140,9 +140,9 @@ USE_TZ = True
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATIC_ROOT = os.path.join(BASE_DIR, 'static_root') #
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') #
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media_root')
 
